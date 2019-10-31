9fcdf2700f965981854a2628cb1dc76a1b8e244b
michelangelo17 <michelangelo.markus@gmail.com>
2019-10-30 22:58:24 -0700

    completed MVP and first stretch goal. NTS: don't finish things and not commit them for hours anymore.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,14 +2,35 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+const cards = document.querySelector('.cards');
+const me = axios.get('https://api.github.com/users/michelangelo17')
+  .then(res => {
+    cards.appendChild(cardMaker(res.data));
+    axios.get('https://api.github.com/users/michelangelo17/following')
+    .then(res => {
+      const urls = res.data.map(person => person.url);
+      urls.forEach(url => {
+        axios.get(url)
+        .then(res => {
+          cards.appendChild(cardMaker(res.data));
+        })
+      })
+    })
+  })
+  .catch(err => {
+    console.log(`${err} FAILED!`);
+  });
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
 
    Skip to Step 3.
 */
-
+// tetondan
+//   dustinmyers
+//   justsml
+//   luishrd
+//   bigknell
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
@@ -24,7 +45,56 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [];
+
+function cardMaker(obj) {
+  //element creator tool
+  const create = el => document.createElement(el);
+  const card = create('div');
+  const usrImg = create('img');
+  const cardInfo = create('div');
+  const name = create('h3');
+  const username = create('p');
+  const location = create('p');
+  const profile = create('p');
+  const profileLink = create('a');
+  const followers = create('p');
+  const following = create('p');
+  const bio = create('p');
+  //class adding tool
+  const addClass = (el, aClass) => el.classList.add(aClass);
+  addClass(card, 'card');
+  addClass(cardInfo, 'card-info');
+  addClass(name, 'name');
+  addClass(username, 'username');
+  //add image
+  usrImg.src = obj.avatar_url;
+  //add profile link
+  profileLink.href = obj.html_url;
+  //textContent tool
+  const text = (el, textToAdd) => el.textContent = textToAdd;
+  text(name, obj.name);
+  text(username, obj.login);
+  obj.location === null ? text(location, `Location: Top Secret`) : text(location, `Location: ${obj.location}`);
+  text(profile, `Profile: `);
+  text(profileLink, `${obj.html_url}`);
+  text(followers, `Followers: ${obj.followers}`);
+  text(following, `Following: ${obj.following}`);
+  obj.bio === null ? text(bio, `This user has no bio!`) : text(bio, `Bio: ${obj.bio}`);
+  //appendChild tool
+  const append = (el, parentEl) => parentEl.appendChild(el);
+  append(usrImg, card);
+  append(cardInfo, card);
+  append(name, cardInfo);
+  append(username, cardInfo);
+  append(location, cardInfo);
+  append(profile, cardInfo);
+  append(followers, cardInfo);
+  append(following, cardInfo);
+  append(bio, cardInfo);
+  append(profileLink, profile);
+  return card;
+}
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
--- README.md
+++ README.md
@@ -36,21 +36,21 @@ In this project we are going to be accessing the GitHub API and building a socia
   * None. We have gone ahead and taken care of this step for you. Focus on the Javascript 😁.
 
 ### Part 1: Requesting Data from the GitHub API
-* [ ] Include the script element linking the `axios` library in your HTML. If you do not remember the code you can find it here: https://github.com/axios/axios
-* [ ] Follow the instructions found in the GitHubCard/index.js file to request data from the GitHub API.
+* [X] Include the script element linking the `axios` library in your HTML. If you do not remember the code you can find it here: https://github.com/axios/axios
+* [X] Follow the instructions found in the GitHubCard/index.js file to request data from the GitHub API.
 
 ### Part 2: Create the component function
 
-* [ ] Once you are receiving data from the GitHub API, take some time to study the data and the information it is giving you. You will create the HTML template you see in the GitHubCard/index.js file and plugging in the dynamic data you are getting from the GitHub API.
-* [ ] Once you complete the component, create a component based on your profile and add it to the DOM.
+* [X] Once you are receiving data from the GitHub API, take some time to study the data and the information it is giving you. You will create the HTML template you see in the GitHubCard/index.js file and plugging in the dynamic data you are getting from the GitHub API.
+* [X] Once you complete the component, create a component based on your profile and add it to the DOM.
 
 ### Part 3: Your Friends
 
-* [ ] After you have successfully added your own card to the DOM, we will get a list of your followers and programmatically add cards for them as well. Follow the instructions in GitHubCard/index.js. 
+* [X] After you have successfully added your own card to the DOM, we will get a list of your followers and programmatically add cards for them as well. Follow the instructions in GitHubCard/index.js. 
 
 ### Stretch Goals:
 
-* [ ] Instead of manually creating a list of followers, do it programmatically. Create a function that requests the followers data from the API after it has received your data and create a card for each of your followers. Hint: you can chain promises.
+* [X] Instead of manually creating a list of followers, do it programmatically. Create a function that requests the followers data from the API after it has received your data and create a card for each of your followers. Hint: you can chain promises.
 
 * [ ] Look into adding more info as an expanding card. You will need to create some new CSS and a button that expands and contracts the card. 
 
